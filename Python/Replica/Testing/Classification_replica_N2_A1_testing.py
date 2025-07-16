c779e6a5e54baf692450a3be7c38dbe0f976ef70
rooomana <mamra2@iscte-iul.pt>
2025-07-16 13:43:52 +0100

    Prep | Added analysis graphs (N2_A1)

--- Python/Replica/Testing/Classification_replica_N2_A1_testing.py
+++ Python/Replica/Testing/Classification_replica_N2_A1_testing.py
@@ -28,6 +28,7 @@ from tensorflow.keras.models import Sequential      # [MR]
 #from tensorflow.keras.layers import Dense           # [MR]
 layers = tf.keras.layers                            # [MR]
 from sklearn.model_selection import StratifiedKFold
+import matplotlib.pyplot as plt                     # [MR] For analysis
 
 ############################## Functions ###############################
 def decode(datum):
@@ -64,6 +65,7 @@ show_inter_results   = 0
 opt = 2     # [MR] DNN Results number
 current_directory_working = os.getcwd()     # [MR] Current working directory
 results_path = rf"{current_directory_working}\Results_{opt}"    # [MR]
+histories = [None] * K  # [MR] For analysis
 
 running_time = {}           # [MR] Timers
 start_time = time.time()    # [MR] Start timer
@@ -92,7 +94,7 @@ cvscores = np.array([])     # [MR]
 
 ## [MR] Each fold's process
 def process_fold(train, test, fold_index, results_lock):
-    global cvscores, running_time
+    global digits_K, cvscores, running_time, histories
     start_time_phase = time.time()  # [MR] Start phase timer
     #cnt = cnt + 1
     #print(f'\n| {cnt = }') # [MR]
@@ -152,8 +154,15 @@ def process_fold(train, test, fold_index, results_lock):
 
     ## [MR] Train and eval model
     # TODO: Fix model training - test with prints
-    model.fit(fold_x[train], y[train], epochs=number_epoch, batch_size=batch_length, verbose=show_inter_results)
+    history = model.fit(
+    fold_x[train], y[train],
+    epochs=number_epoch,
+    batch_size=batch_length,
+    verbose=show_inter_results,
+    validation_data=(fold_x[test], y[test])  # [MR] For analysis
+    )
     #train_step(model, fold_x[train], y[train])
+    histories[fold_index - 1] = history.history  # [MR] For analysis
     scores = model.evaluate(fold_x[test], y[test], verbose=show_inter_results)
     print(f'\n| Fold {fold_index:>{digits_K}} | Scores = {scores[1] * 100}') # [MR]
     
@@ -201,3 +210,69 @@ print('\nRunning Time:\n')
 for phase_name, phase_elapsed_time in running_time.items():
     print(f'| {phase_name:<{longest_name_length}} = {phase_elapsed_time:>{longest_time_length}.4f} seconds')
 print('\n')
+
+#########################################################################
+# [MR] Graphs for analysis
+
+# Accuracy
+train_accuracies = []
+valid_accuracies = []
+# Loss
+train_losses     = []
+valid_losses     = []
+
+# Join histories from each fold
+for fold_index, history in enumerate(histories):
+    if history is None: continue
+    train_accuracies.append(history["accuracy"])
+    train_losses.append(history["loss"])
+    if "val_accuracy" in history:
+        valid_accuracies.append(history["val_accuracy"])
+    if "val_loss" in history:
+        valid_losses.append(history["val_loss"])
+
+## Means
+# Accuracy
+mean_train_accuracies = np.mean(np.array(train_accuracies), axis=0)
+mean_valid_accuracies = np.mean(np.array(valid_accuracies), axis=0)
+# Loss
+mean_train_loss       = np.mean(np.array(train_losses), axis=0)
+mean_valid_loss       = np.mean(np.array(valid_losses), axis=0)
+
+## Plot
+plt.figure(figsize=(12, 6))
+
+# Accuracy | Train
+plt.subplot(2, 2, 1)
+plt.plot(mean_train_accuracies, color='red')
+plt.title("Accuracy | Train")
+plt.ylabel("Accuracy")
+plt.xlabel("Epoch")
+plt.legend()
+
+# Loss | Train
+plt.subplot(2, 2, 2)
+plt.plot(mean_train_loss, color='blue')
+plt.title("Loss | Train")
+plt.ylabel("Loss")
+plt.xlabel("Epoch")
+plt.legend()
+
+# Accuracy | Validation
+plt.subplot(2, 2, 3)
+plt.plot(mean_valid_accuracies, color='red')
+plt.title("Accuracy | Validation")
+plt.ylabel("Accuracy")
+plt.xlabel("Epoch")
+plt.legend()
+
+# Loss | Validation
+plt.subplot(2, 2, 4)
+plt.plot(mean_valid_loss, color='blue')
+plt.title("Loss | Validation")
+plt.ylabel("Loss")
+plt.xlabel("Epoch")
+plt.legend()
+
+plt.tight_layout()
+plt.show()

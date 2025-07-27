4187f0e05dbf31e2185ab1161f60b781a63d64a2
rooomana <mamra2@iscte-iul.pt>
2025-07-27 13:46:13 +0100

    Prep | Set parameters

--- Python/Replica/Testing/Classification_replica_N1_A2_testing.py
+++ Python/Replica/Testing/Classification_replica_N1_A2_testing.py
@@ -117,7 +117,7 @@ def process_fold(train, test, fold_index, results_lock):
             strides=1,
             padding='same',
             activation='relu',
-            dilation_rate=3
+            dilation_rate=2
         ))
         model.add(layers.AveragePooling1D(pool_size=3))
     

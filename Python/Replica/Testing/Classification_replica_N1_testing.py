9eeb6e009c0f5f8f63f68fce0a552f898361ff1b
rooomana <mamra2@iscte-iul.pt>
2025-10-08 12:07:04 +0100

    Prep | Set parameters [RNN]

--- Python/Replica/Testing/Classification_replica_N1_testing.py
+++ Python/Replica/Testing/Classification_replica_N1_testing.py
@@ -111,11 +111,11 @@ def process_fold(train, test, fold_index, results_lock):
     ### T3: 1-LSTM [4-epoch] return_sequences=False w/o pooling w/o dropout
     ### F1: Execute 1-LSTM w/ 200 epoch
     ### F2: Execute 2-LSTM w/ 200 epoch
-    model.add(layers.LSTM(64, activation='tanh', return_sequences=True))
+    model.add(layers.LSTM(64, activation='tanh', return_sequences=False))
     #model.add(layers.LSTM(32, activation='tanh', return_sequences=False))
 
     # Pooling layers
-    model.add(layers.GlobalAveragePooling1D())
+    #model.add(layers.GlobalAveragePooling1D())
 
     # Dropout to prevent overfitting
     #model.add(layers.Dropout(0.25))
@@ -158,11 +158,9 @@ def process_fold(train, test, fold_index, results_lock):
     ## [MR] Save results
     y_pred = model.predict(fold_x[test])    # [MR]
     # [MR] Debugging
-    # If all ~0.5 - underfits
-    # If all ~0 or ~1 - data imbalance
-    print(f'\n| Mean | {np.mean(y_pred)}') # [MR]
-    print(f'\n| Min  | {np.min(y_pred)}') # [MR]
-    print(f'\n| Max  | {np.max(y_pred)}') # [MR]
+    print(f'\n| Fold {fold_index:>{digits_K}} | Mean | {np.mean(y_pred):.6f}') # [MR]
+    print(f'\n| Fold {fold_index:>{digits_K}} | Min  |  {np.min(y_pred):.6f}') # [MR]
+    print(f'\n| Fold {fold_index:>{digits_K}} | Max  |  {np.max(y_pred):.6f}') # [MR]
     # [MR] (Results_{1,2,3} - Demo_4) - Only saving results for the 3rd NN (?)
     # np.savetxt("Results_3%s.csv" % cnt, np.column_stack((y[test], y_pred)), delimiter=",", fmt='%s')
     results_file = rf"{results_path}\Results_{opt}{fold_index}.csv"     # [MR] Saved results path
